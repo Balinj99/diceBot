e1024b129782bfa8ca960ca03b26f3bd37246307
Balinj99 <balinjacob@gmail.com>
2022-10-02 00:22:25 -0700

    All v1 bot code.

--- main.py
+++ main.py
@@ -1,5 +1,60 @@
-def main():
-    print("hello")
+import discord
+import os
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+def rollDice(num, dice, mod):
+  print("num: ", num)
+  print("dice: ", dice)
+  print("mod: ", mod)
+  res = 0  
+  for i in range(num):
+      n = random.randint(1, dice)
+      print("roll: ", n)
+      res += n
+
+  res += mod
+  print("res: ", res)
+  
+  return res
+
+@client.event
+async def on_ready():
+  print('DiceBot is live')
+  
+@client.event
+async def on_message(message):
+  msg = message.content
+  if message.author == client.user:
+    return
+
+  if msg.startswith('!ping'):
+    await message.channel.send('pong')
+  
+  elif msg.startswith('!'):
+    str = msg.split('!')
+    res = None
+    mod = 0
     
-if __name__ == "__main__":
-    main()
+    try:
+      roll = str[1].split('d')
+      num = int(roll[0])
+      try:
+        d = roll[1].split(' ')
+        dice = int(d[0])
+        mod = int(d[1])
+      except:
+        dice = int(roll[1])
+      res = rollDice(num, dice, mod)
+    except:
+      res = None
+
+    if res == None:
+      await message.channel.send('Oops')
+    else:
+      await message.channel.send(f'{res}')
+    
+client.run(os.environ['TOKEN'])
